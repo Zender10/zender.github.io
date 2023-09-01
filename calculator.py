1e95974e998d6e5cfb93bcda3c145df3c7108bd7
Zender10 <143846637+Zender10@users.noreply.github.com>
2023-09-02 00:30:48 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# Define a dictionary to map operators to functions
+operators = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+
+# Input
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Check if the operator is valid
+if operator in operators:
+    result = operators[operator](num1, num2)
+    print(f"Result: {result}")
+else:
+    print("Invalid operator")
